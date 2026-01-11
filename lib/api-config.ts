310a6984bac630b0ef7d36a2d480174630e40c1c
Asadbek <asadbekabdunazarov602@gmail.com>
2026-01-11 12:38:26 +0500

    Upload project

--- lib/api-config.ts
+++ lib/api-config.ts
@@ -9,9 +9,10 @@
  */
 
 // Bitta joydan boshqarish uchun - faqat shu yerda o'zgartiring
-export const DEFAULT_API_BASE_URL = 'https://api.backend.autonline.uz'
+export const DEFAULT_API_BASE_URL = 'https://autonline-backend-production.up.railway.app'
 //http://localhost:8080
 //https://autonline-backend-production.up.railway.app
+//https://api.backend.autonline.uz
 /**
  * Get API base URL from environment variable or use default
  * Environment variable ustunlik qiladi
